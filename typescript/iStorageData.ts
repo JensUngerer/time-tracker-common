6823b6020171e7fbfc9816433e03924e575af296
Jens Ungerer <github@jens-ungerer.de>
2019-07-16 14:53:23 +0200

    adding and using ITask

--- typescript/iStorageData.ts
+++ typescript/iStorageData.ts
@@ -1,7 +1,9 @@
 import { IUser } from './iUser';
 import { IProject } from './iProject';
+import { ITask } from './iTask';
 
 export interface IStorageData {
     users: IUser[];
     projects: IProject[];
+    tasks: ITask[];
 }
