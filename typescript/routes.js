42572caadc5ea396a025e9f5d1ae918019e9e9c1
Jens Ungerer <github@jens-ungerer.de>
2019-07-20 14:48:17 +0200

    extending config

--- typescript/routes.js
+++ typescript/routes.js
@@ -2,6 +2,7 @@
 // https://stackoverflow.com/questions/44817349/how-to-include-external-js-file-in-angular-4-and-call-function-from-angular-to-j
 // https://stackoverflow.com/questions/25574826/how-to-requiremodule-in-mongo-shell
 var routes = {
+    url: 'mongodb://localhost:27017',
     port: 3000,
     databaseName: 'timeTracker',
 
