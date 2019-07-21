cf6e52e13b751deb4d667f6e23d430e2ac362a08
Jens Ungerer <github@jens-ungerer.de>
2019-07-21 09:36:26 +0200

    introducing prefix of html-page-routes in config

--- typescript/routes.js
+++ typescript/routes.js
@@ -2,6 +2,8 @@
 // https://stackoverflow.com/questions/44817349/how-to-include-external-js-file-in-angular-4-and-call-function-from-angular-to-j
 // https://stackoverflow.com/questions/25574826/how-to-requiremodule-in-mongo-shell
 var routes = {
+    viewsPrefix: 'view/',
+
     url: 'mongodb://localhost:27017',
     port: 3000,
     databaseName: 'timeTracker',
