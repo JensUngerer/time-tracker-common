b7d85848aeb56f8516f8ef553a69d680fca6a0ab
Jens Ungerer <github@jens-ungerer.de>
2019-07-27 13:54:33 +0200

    adjusting data structure for TimeRecord

--- typescript/mongoDB/iTimeRecordsDocument.ts
+++ typescript/mongoDB/iTimeRecordsDocument.ts
@@ -1,18 +1,19 @@
 import { IDate } from './../iDate';
 import { IDuration } from './../iDuration';
 
-export interface IExtendedTimeRecordsDocumentData {
-    data: ITimeRecordsDocumentData;
-    timeEntryIds: string[];    
-}
+// export interface IExtendedTimeRecordsDocumentData {
+//     data: ITimeRecordsDocumentData;
+//     timeEntryIds: string[];    
+// }
 
 export interface ITimeRecordsDocumentData {
     durationStructure: IDuration;
     dateStructure: IDate;
-    _taskIds: string[];
-    _projectId: string;
+    // _taskIds: string[];
+    // _projectId: string;
+    _timeEntryIds: string[];
 }
 
 export interface ITimeRecordsDocument extends ITimeRecordsDocumentData {
-    _id: string;        
+    _id: string;
 }
