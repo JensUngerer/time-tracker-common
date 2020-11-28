c6ef975db96de264f2a57a4f979e948816782077
Jens Ungerer <github@jens-ungerer.de>
2020-11-28 04:09:42 +0100

    more properties in constants

--- typescript/constants.ts
+++ typescript/constants.ts
@@ -5,4 +5,13 @@ export class Constants {
     static HOURS_IN_DAY = 24;
 
     static HOURS_IN_MILLISECONDS = (1000 * 60 * 60);
+
+    static formatDateDay = 'yyy-MM-dd';
+    static formatDateTime = 'HH:mm:ss';
+  
+    static formatPercent = '2.0-0';
+
+    static formatNumber = '1.2-2';
+    
+    static formatMilliseconds: '7.0-0';  
 }
