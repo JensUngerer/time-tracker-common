b13a12b64257e4f842198660a6f455ae1c6f6452
Jens Ungerer <github@jens-ungerer.de>
2021-02-21 15:38:28 +0100

    using library method toMillis() (instead of implementing it "by own")

--- typescript/helpers/durationHelper.ts
+++ typescript/helpers/durationHelper.ts
@@ -1,26 +1,7 @@
 import { Duration } from 'luxon';
-import { Constants } from './../constants';
 
 export class DurationHelper {
     static durationToMillis(duration: Duration) {
-        const days = duration.days;
-        const hours = duration.hours;
-        const minutes = duration.minutes;
-        const seconds = duration.seconds;
-        const milliseconds = duration.milliseconds;
-
-        let millisecondsSum = 0;
-
-        millisecondsSum += milliseconds;
-
-        millisecondsSum += seconds * Constants.MILLISECONDS_IN_SECOND;
-
-        millisecondsSum += minutes * Constants.MILLISECONDS_IN_MINUTE;
-
-        millisecondsSum += hours * Constants.MILLISECONDS_IN_HOUR;
-
-        millisecondsSum += days * Constants.MILLISECONDS_IN_DAY;
-
-        return millisecondsSum;
+        return duration.toMillis();
     }
 }
